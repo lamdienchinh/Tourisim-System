23bb9b7e750f6b3f9e921494c7e8d6e4763393f5
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2023-07-12 15:55:22 +0700

    Modified UI

--- src/layouts/header/index.js
+++ src/layouts/header/index.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 // import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { connectWallet } from '../../state/userSlice';
@@ -9,21 +10,43 @@ import { Avatar } from "@mui/material";
 import Container from '@mui/material/Container';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Sidebar from "../sidebar";
 import "./css/Header.scss";
 import logo from "../../assets/imgs/logo1.png"
 import avatar from "../../assets/imgs/avatar.png";
 import { FaCamera } from 'react-icons/fa';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Fade from '@mui/material/Fade';
+import { logout } from "../../state/userSlice";
+import Draggable from 'react-draggable';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Paper } from '@mui/material'
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+function PaperComponent(props) {
+    return (
+        <Draggable
+            handle="#draggable-dialog-title"
+            cancel={'[class*="MuiDialogContent-root"]'}
+        >
+            <Paper {...props} />
+        </Draggable>
+    );
+}
 
 const Header = () => {
     //Connect Wallet
-    const dispatch = useDispatch();
     let walletAddress = useSelector(getUserData);
     let image = avatar;
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [clogout, setClogout] = useState(false);
+
     const login = async () => {
-        if (walletAddress) navigate("/user");
-        else {
+        if (walletAddress === "") {
+            console.log(walletAddress)
             dispatch(connectWallet(dispatch));
             // toast.success('Kết nối ví Metamask thành công !');
         }
@@ -41,6 +64,33 @@ const Header = () => {
             console.error('Lỗi truy cập camera:', error);
         }
     };
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = (event, selection) => {
+        console.log(selection)
+        if (selection === "profile") {
+            navigate("/user")
+        }
+        else if (selection === "album") {
+            navigate("/album")
+        }
+        else if (selection === "logout") {
+            setClogout(true);
+        }
+        setAnchorEl(null);
+    };
+
+    const confirmlogout = (event, check) => {
+        if (check) {
+            dispatch(logout(dispatch));
+            navigate('/home')
+        }
+        setClogout(false);
+    }
+
     return (
         <header className="header">
             <ToastContainer position="top-right"
@@ -68,13 +118,9 @@ const Header = () => {
                         </NavLink>
                     </div>
                     <div className="header__ele header__ele--home">
-                        <NavLink className={({ isActive, isPending }) => isPending ? "header__link" : isActive ? "header__link--selected" : "header__link"} to='/home'>
+                        <NavLink className={({ isActive, isPending }) => isPending ? "header__link" : isActive ? "header__link--selected" : "header__link"} to='/travel'>
                             TRAVEL
                         </NavLink>
-                    </div>
-                    <div className={walletAddress && walletAddress.length > 0 ? "header__ele header__ele--connected" : "header__ele header__ele--login"} onClick={login}>
-                        {walletAddress && walletAddress.length > 0
-                            ? <Avatar className="header__avatar" src={image} alt="Avatar" /> : "LOGIN"}
                     </div>
                     {
                         walletAddress && walletAddress.length > 0 ?
@@ -82,22 +128,74 @@ const Header = () => {
                                 <div className="user__camera">
                                     <button className="camera-button" onClick={openCamera}>
                                         <FaCamera className="camera-icon" />
-                                        Mở camera
+                                        CAMERA
                                     </button>
                                 </div>
                             </div>
                             : ""
                     }
-                    {
-                        walletAddress && walletAddress.length > 0 ?
-                            <div>
-                                <Sidebar></Sidebar>
-                            </div>
-                            : ""
-                    }
+                    <div className={walletAddress && walletAddress.length > 0 ? "header__ele header__ele--connected" : "header__ele header__ele--login"} onClick={login}>
+                        {walletAddress && walletAddress.length > 0
+                            ? <Avatar className="header__avatar" src={image} alt="Avatar" aria-controls={open ? 'fade-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick} /> : "LOGIN"}
+                        <Menu
+                            id="fade-menu"
+                            MenuListProps={{
+                                'aria-labelledby': 'fade-button',
+                            }}
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            TransitionComponent={Fade}
+                            disableScrollLock={true}
+                        >
+                            <MenuItem onClick={(event) => handleClose(event, "profile")}>
+                                <div className="menu-wrapper">
+                                    <AccountBoxIcon />
+                                    Profile
+                                </div>
+                            </MenuItem>
+                            <MenuItem onClick={(event) => handleClose(event, "album")}>
+                                <div className="menu-wrapper">
+                                    <CollectionsIcon />
+                                    Album
+                                </div>
+                            </MenuItem>
+                            <MenuItem onClick={(event) => handleClose(event, "logout")}>
+                                <div className="menu-wrapper">
+                                    <LogoutIcon />
+                                    Logout
+                                </div>
+                            </MenuItem>
+                        </Menu>
+                    </div>
                 </div>
             </Container>
-        </header>
+            <Dialog
+                disableScrollLock={true}
+                open={clogout}
+                onClose={confirmlogout}
+                PaperComponent={PaperComponent}
+                aria-labelledby="draggable-dialog-title"
+            >
+                <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+                    Đăng xuất
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Bạn có muốn đăng xuất không, hãy xác nhận thật kỹ?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={(event) => confirmlogout(event, 0)}>
+                        Huỷ
+                    </Button>
+                    <Button onClick={(event) => confirmlogout(event, 1)}>Xác nhận</Button>
+                </DialogActions>
+            </Dialog>
+        </header >
     );
 }
 
